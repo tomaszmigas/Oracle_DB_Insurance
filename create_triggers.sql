969ddbc82ec60a224ea71649cce29bf498d5ea9d
Tomasz Migas <tmigas79@gmail.com>
2023-05-04 23:02:36 +0200

    fix(trigger): LOGON LOGOFF Trigger dynamiczna nazwa schematu
    Zmiana stałej nazwy schematu w definicji triggerów na dynamiczną zależną od nazwy usera

--- create_triggers.sql
+++ create_triggers.sql
@@ -1,7 +1,6 @@
 PROMPT Tworzenie triggera Schema Logon Check...
-
 create or replace trigger trig_schema_logon_check
-AFTER LOGON ON INS4.SCHEMA
+AFTER LOGON ON &&v_user..SCHEMA
 BEGIN
     INSERT INTO info_log ("EVENT","DATA","TIME","USER",DB_NAME,"SESSION_ID",IP_ADDRESS,NETWORK_PROTOCOL)
     select 
@@ -16,9 +15,10 @@ BEGIN
     from dual;
 END trig_schema_logon_check;
 /
+
 PROMPT Tworzenie triggera Schema Logoff Check...
 create or replace trigger trig_schema_logoff_check
-BEFORE LOGOFF ON INS4.SCHEMA
+BEFORE LOGOFF ON &&v_user..SCHEMA
 BEGIN
     INSERT INTO info_log ("EVENT","DATA","TIME","USER",DB_NAME,"SESSION_ID",IP_ADDRESS,NETWORK_PROTOCOL)
     select 
@@ -35,8 +35,21 @@ END trig_schema_logoff_check;
 /
 
 create or replace TRIGGER CHANGE_DATE_FORMAT
-AFTER LOGON ON INS4.SCHEMA
+AFTER LOGON ON &&v_user..SCHEMA
 begin
 	DBMS_SESSION.SET_NLS('NLS_DATE_FORMAT','"YYYY/MM/DD"');
 end CHANGE_DATE_FORMAT;
+/
+
+PROMPT Tworzenie triggera Kontrahenci_Check...
+create or replace trigger trig_kontrahenci_check BEFORE INSERT ON szkody FOR EACH ROW
+DECLARE
+	v_ilosc number;
+BEGIN
+	SELECT COUNT(nr_polisy) into v_ilosc from kontrahenci where nr_polisy = :NEW.nr_polisy;
+	IF v_ilosc = 0 THEN
+		RAISE_APPLICATION_ERROR(-20998,'Trig Exception - brak odpowiedniego wpisu w tabeli kontrahenci');
+	END IF;
+	
+END trig_kontrahenci_check;
 /
