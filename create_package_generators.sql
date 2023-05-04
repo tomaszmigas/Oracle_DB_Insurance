3bf7e8e23911aabb88e5ce915aa641d00cfc3e5f
Tomasz Migas <tmigas79@gmail.com>
2023-05-04 22:57:28 +0200

    fix(Generuj_Pesel): poprawa wyliczania numeru PESEL
    1. prawidłowe obliczanie cyfry kontrolnej dla przypadku 10-0
    2. losowa liczna z zakresu 0-9999 wyświetlana zawsze na 4 polach

--- create_package_generators.sql
+++ create_package_generators.sql
@@ -1,3 +1,4 @@
+PROMPT Tworzenie pakietu Generatory (spec)...
 CREATE OR REPLACE 
 PACKAGE GENERATORY_PKG AS 
     TYPE wiersz is RECORD (
@@ -8,7 +9,7 @@ PACKAGE GENERATORY_PKG AS
     
     function generuj_date(p_data_od date, p_data_do date) return date;
     function generuj_pesel(p_data_od date, p_data_do date) return varchar2;
-    function generuj_dane_osobowe(p_plec CHAR) return wiersz;
+--    function generuj_dane_osobowe(p_plec CHAR) return wiersz;
     function generuj_dane_osobowe(p_pesel VARCHAR2) return wiersz;
     function generuj_sume_ubezp(p_suma_min number, p_suma_max number) return number;
      
@@ -18,7 +19,7 @@ PACKAGE GENERATORY_PKG AS
 END GENERATORY_PKG;
 /
 ----------------------------------------
-
+PROMPT Tworzenie pakietu Generatory (body)...
 CREATE OR REPLACE PACKAGE BODY GENERATORY_PKG AS
 -----------------------------------------
   function generuj_date(p_data_od date, p_data_do date) return date AS
@@ -35,14 +36,16 @@ CREATE OR REPLACE PACKAGE BODY GENERATORY_PKG AS
 -----------------------------------------
 
 -----------------------------------------
-  function generuj_pesel(p_data_od date, p_data_do date) return varchar2 AS
+ function generuj_pesel(p_data_od date, p_data_do date) return varchar2 AS
     v_data date;
     v_pesel varchar2(20);
     v_liczba_kontrolna number:=0;
     v_mnoznik number;
+    v_temp number;
+    v_temp2 varchar2(10);
   BEGIN
     v_data:=generuj_date(p_data_od, p_data_do);
-    dbms_output.put_line('Data: ' || v_data);
+--    dbms_output.put_line('Data: ' || v_data);
     IF EXTRACT(year from v_data) > 1899  AND EXTRACT(year from v_data) < 2000 THEN
         v_pesel:= to_char(v_data,'YYMMDD');
     ELSIF EXTRACT(year from v_data) < 2100 THEN
@@ -50,7 +53,8 @@ CREATE OR REPLACE PACKAGE BODY GENERATORY_PKG AS
     ELSE 
         RAISE_APPLICATION_ERROR(-20999,'Nieobsługiwany zakres dat');
     END IF;
-        v_pesel:=v_pesel || trunc(dbms_random.value(0,9999));
+        v_pesel:=v_pesel || LTRIM(to_char(trunc(dbms_random.value(1,9999)),'0000'));
+        dbms_output.put_line( 'v_pesel: ' || v_pesel);   
         for i in 1..10 LOOP
             v_mnoznik:=case
                 WHEN i = 1 OR i=5 OR i=9    THEN 1
@@ -58,10 +62,17 @@ CREATE OR REPLACE PACKAGE BODY GENERATORY_PKG AS
                 WHEN i = 3 OR i=7           THEN 7
                 WHEN i = 4 OR i=8           THEN 9
             end;
+         dbms_output.put_line( 'Skladnik(' || i || '): ' || substr(v_pesel,i,1) || ' * ' || v_mnoznik || ' = '    || substr(substr(v_pesel,i,1)*v_mnoznik,-1,1));   
         v_liczba_kontrolna:=v_liczba_kontrolna + substr(substr(v_pesel,i,1)*v_mnoznik,-1,1);
             --wynik jako suma mnozenia skladowych pesela przez wagi (tylko pola jednosci biorą udział)
         END LOOP;
-        v_liczba_kontrolna:=10-substr(v_liczba_kontrolna,-1,1); --bierzemy tylko cyfrę jednosci
+        dbms_output.put_line( 'Suma dla liczby kontrolnej: ' || v_liczba_kontrolna);   
+        dbms_output.put_line( 'Obcieta Suma dla liczby kontrolnej: ' || substr(v_liczba_kontrolna,-1,1));   
+        IF substr(v_liczba_kontrolna,-1,1) = 0 THEN --gdy suma to pelne dziesiatki (ryzyko 12 znakow 
+            v_liczba_kontrolna:=0;
+        ELSE
+            v_liczba_kontrolna:=10-substr(v_liczba_kontrolna,-1,1); --bierzemy tylko cyfrę jednosci
+        END IF;
         v_pesel:=v_pesel || v_liczba_kontrolna;
         
     RETURN v_pesel;
@@ -75,28 +86,54 @@ CREATE OR REPLACE PACKAGE BODY GENERATORY_PKG AS
 -----------------------------------------
 
 -----------------------------------------
+/*
   function generuj_dane_osobowe(p_plec CHAR) return wiersz AS
   BEGIN
     -- TODO: Implementation required for function GENERATORY_PKG.generuj_dane_osobowe
     RETURN NULL;
   END generuj_dane_osobowe;
-----------------------------------------
+*/
+-----------------------------------------
 
 -----------------------------------------
 function generuj_dane_osobowe(p_pesel VARCHAR2) return wiersz AS
+    v_plec char;
+    v_dane wiersz;
+    v_zakres integer;
+    v_pozycja integer;
   BEGIN
-    -- TODO: Implementation required for function GENERATORY_PKG.generuj_dane_osobowe
-    RETURN NULL;
+        -- jezeli 10 cyfra numeru pesel pokazuje na kobiete
+    IF substr(p_pesel,10,1) IN (0,2,4,6,8) THEN 
+          select count(*) into v_zakres from imiona_zenskie; 
+          select dbms_random.value(1,v_zakres) into v_pozycja from dual;
+          SELECT imie into v_dane.imie from imiona_zenskie where id = v_pozycja;
+          
+          select count(*) into v_zakres from nazwiska_zenskie; 
+          select dbms_random.value(1,v_zakres) into v_pozycja from dual;
+          SELECT nazwisko into v_dane.nazwisko from nazwiska_zenskie where id = v_pozycja;
+          v_dane.pesel:=p_pesel;
+    ELSE
+            -- gdy pesel pokazuje na mezczyzne
+          select count(*) into v_zakres from imiona_meskie; 
+          select dbms_random.value(1,v_zakres) into v_pozycja from dual;
+          SELECT imie into v_dane.imie from imiona_meskie where id = v_pozycja;
+          
+          select count(*) into v_zakres from nazwiska_meskie; 
+          select dbms_random.value(1,v_zakres) into v_pozycja from dual;
+          SELECT nazwisko into v_dane.nazwisko from nazwiska_meskie where id = v_pozycja;
+          v_dane.pesel:=p_pesel;
+        END IF;
+    RETURN v_dane;
   END generuj_dane_osobowe;
-  -----------------------------------------
-
+-----------------------------------------
 
 
 -----------------------------------------
   function generuj_sume_ubezp(p_suma_min number, p_suma_max number) return number AS
+    v_suma number:=0;
   BEGIN
-    -- TODO: Implementation required for function GENERATORY_PKG.generuj_sume_ubezp
-    RETURN NULL;
+    v_suma:= trunc(dbms_random.value(p_suma_min,p_suma_max));
+    RETURN v_suma;
   END generuj_sume_ubezp;
 -----------------------------------------
 END GENERATORY_PKG;
