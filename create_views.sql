b6b84d43f4d379cbbd36963bf2fa61c117185dc5
Tomasz Migas <tmigas79@gmail.com>
2023-05-07 15:36:49 +0200

    feat(views): Dodanie 11 widoków do bazy
    01	v_agenci_wskazniki
    02	v_polisy_bez_szkod
    03	v_polisy_indywidualne
    04	v_polisy_indywidualne_wlascicieli
    05	v_polisy_osoby
    06	v_polisy_przekroczona_wartosc
    07	v_polisy_wlasciciele
    08	v_szkody_opoznione
    09	v_szkody_przeterminowane
    10	v_szkody_wysokie
    11	v_wskazniki_szkodliwosci

---
+++ create_views.sql
@@ -0,0 +1,96 @@
+PROMPT Tworzenie widok¢w...
+
+CREATE VIEW v_polisy_osoby AS
+SELECT imie, nazwisko, pesel, (SELECT COUNT(*) FROM kontrahenci k WHERE o.id_osoby = k.id_osoby AND id_roli =1) ilosc_polis 
+FROM osoby o
+;
+
+
+CREATE VIEW v_polisy_wlasciciele AS
+WITH 
+    k AS (SELECT count(nr_polisy) ilosc_polis, id_osoby FROM kontrahenci WHERE id_roli =1 GROUP BY id_osoby)
+SELECT 
+    imie, nazwisko, pesel, ilosc_polis FROM osoby o
+INNER JOIN k USING(id_osoby)
+;
+
+
+CREATE VIEW v_polisy_indywidualne_wlascicieli AS
+SELECT nr_polisy from polisy p
+INNER JOIN kontrahenci k1 using (nr_polisy)
+INNER JOIN kontrahenci k2 using (nr_polisy)
+INNER JOIN (SELECT count(*) ilosc, nr_polisy FROM kontrahenci k GROUP BY nr_polisy) k3 USING (nr_polisy)
+WHERE k1.id_roli=1
+AND k2.id_roli=1
+AND k3.ilosc=2
+;
+
+
+CREATE VIEW v_polisy_indywidualne AS
+SELECT nr_polisy FROM
+(SELECT count(*) ilosc, nr_polisy FROM kontrahenci k GROUP BY nr_polisy) k3 
+WHERE 
+k3.ilosc=2
+;
+
+CREATE OR REPLACE VIEW v_polisy_bez_szkod AS
+select nr_polisy Polisy_bez_szkod from polisy 
+minus 
+select distinct nr_polisy from szkody;
+
+CREATE VIEW v_polisy_przekroczona_wartosc AS
+SELECT nr_polisy, suma_ubezpieczenia, sum(wartosc_wyplaty) AS suma_wyplat FROM szkody
+INNER JOIN polisy USING (nr_polisy)
+GROUP BY suma_ubezpieczenia, nr_polisy
+HAVING SUM(wartosc_wyplaty) > suma_ubezpieczenia
+;
+
+
+create view v_agenci_wskazniki AS
+WITH
+    wszystkie_polisy AS (SELECT count(nr_polisy) AS ilosc, nr_agenta FROM polisy GROUP BY nr_agenta)
+    ,akt_polisy AS (SELECT count(nr_polisy) AS ilosc, nr_agenta FROM polisy WHERE data_do>=current_date GROUP BY nr_agenta)
+SELECT nr_agenta, nazwa, NVL(wszystkie_polisy.ilosc,0) polisy
+,NVL(round(akt_polisy.ilosc/wszystkie_polisy.ilosc*100,1),0) || '%' AS wsk_akt_polis FROM agenci
+LEFT JOIN wszystkie_polisy using (nr_agenta)
+LEFT JOIN akt_polisy using  (nr_agenta)
+;
+
+CREATE VIEW v_szkody_przeterminowane AS
+SELECT nr_polisy,data_do WA½NO—_POLISY, data_zgloszenia, trunc(data_zgloszenia-data_do) dni_przekroczenia FROM szkody
+INNER JOIN polisy USING (nr_polisy)
+WHERE data_zgloszenia>data_do
+;
+
+CREATE VIEW v_szkody_wysokie AS
+select count(*) liczba_szkod, sum(wartosc_wyplaty) suma, pesel from szkody
+inner join polisy p using(nr_polisy)
+inner join kontrahenci k using (nr_polisy)
+inner join osoby o using (id_osoby)
+inner join rola r using (id_roli)
+where r.nazwa = 'ubezpieczaj¥cy'
+group by pesel
+having sum(wartosc_wyplaty)>20000
+;
+
+CREATE VIEW v_szkody_opoznione AS
+select  nr_polisy,data_zgloszenia,nazwa , trunc(current_date - data_zgloszenia) dni
+from szkody
+inner join szkody_status using (id_status)
+where nazwa NOT IN ('wypˆacona','odrzucona')
+AND (current_date - data_zgloszenia) >7
+;
+
+CREATE VIEW V_WSKAZNIKI_SZKODLIWOSCI AS
+WITH
+     p1 AS (select count(distinct nr_polisy) ilosc from szkody)
+    ,p2 AS (select count(distinct nr_polisy) ilosc from polisy)
+    ,w1 AS (select sum(wartosc_wyplaty) suma from szkody)
+    ,w2 AS (select sum(suma_ubezpieczenia) suma from polisy p where exists (SELECT 1 FROM szkody s WHERE p.nr_polisy = s.nr_polisy))
+SELECT 
+ round((p1.ilosc/p2.ilosc)*100,2)|| '%' "% POLIS ZE SZKODAMI"
+,round((w1.suma/w2.suma)*100,2) || '%' " KWOTA ODSZK/UBEZP"
+FROM p1,p2,w1,w2;
+
+
+
