09ba43210cc0b4ca82d0051fc898920777790fc1
Tomasz Migas <tmigas79@gmail.com>
2023-05-07 23:30:30 +0200

    feat(mview): Dodanie widoku zmaterializowanego pokazujacego konczace sie polisy (7 dni)

---
+++ create_materialized_views.sql
@@ -0,0 +1,17 @@
+PROMPT Tworzenie widokow zmaterializowanych...
+
+create materialized view mv_polisy_koniec
+refresh force on demand
+start with sysdate
+next sysdate+1
+AS 
+SELECT nr_polisy, data_do, current_date, imie, nazwisko,trunc(data_do - current_date) dni
+FROM polisy
+INNER JOIN kontrahenci using(nr_polisy)
+INNER JOIN osoby using (id_osoby)
+INNER JOIN rola using(id_roli)
+WHERE 
+nazwa = 'ubezpieczaj¥cy'
+AND (data_do - current_date) between 0 and 7
+order by dni desc
+;
