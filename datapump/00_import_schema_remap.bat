2faf4c451670f4cd6667c98e38bbf40f4e41786c
Tomasz Migas <tmigas79@gmail.com>
2023-05-08 16:49:49 +0200

    fix(datapump): "Aktualizacja pliku dmp expdp + Opis"

--- datapump/00_import_schema_remap.bat
+++ datapump/00_import_schema_remap.bat
@@ -1,3 +1,3 @@
 @echo off
 rem  impdp ins/ins@xepdb3 PARFILE = parfile_schema.par
-impdp 'sys@xepdb3 as sysdba' directory = ins_datapump dumpfile = insurance_schema_2023-05-08__09_25_12.dmp logfile = import.log remap_schema = INS:INS3
+impdp 'sys@xepdb3 as sysdba' directory = ins_datapump dumpfile = INSURANCE_SCHEMA_2023-05-08__16_12_06.DMP logfile = import.log remap_schema = INS:INS
